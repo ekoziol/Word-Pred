39b63d32e8eb3ab8530c18fa0ed09593d32e87a7
Eric Koziol <e.trip.k@gmail.com>
2014-11-03 18:51:20 -0700

    Added Exploration

---
+++ Exploration.R
@@ -0,0 +1,5 @@
+require(tm)
+#load in the english blog data
+data_blogs = readLines("Raw_Data/en_US/en_US.blogs.txt")
+data_twitter=readLines("Raw_Data/en_US/en_US.twitter.txt")
+data_news=readLines("Raw_Data/en_US/en_US.news.txt")
