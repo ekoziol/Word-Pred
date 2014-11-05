adab00e2ef457df9386b75317528c84a3a9f856b
ekoziol <e.trip.k@gmail.com>
2014-11-04 20:35:26 -0600

    Added Profanity Words

--- Exploration.R
+++ Exploration.R
@@ -1,5 +1,8 @@
+setwd("D:/Github/SwiftKey_Prediction")
+
 require(tm)
 #load in the english blog data
 data_blogs = readLines("Raw_Data/en_US/en_US.blogs.txt")
 data_twitter=readLines("Raw_Data/en_US/en_US.twitter.txt")
 data_news=readLines("Raw_Data/en_US/en_US.news.txt")
+crps  <- Corpus(DirSource("D:/Github/SwiftKey_Prediction/Raw_Data/en_US/", encoding="UTF-8"), readerControl = list(language="en_US"))
