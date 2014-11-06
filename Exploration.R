cd0c0642e941e4a8b9ed72ddf8d8f4745c31925a
ekoziol <e.trip.k@gmail.com>
2014-11-05 21:37:22 -0600

    Added data cleaning and profanity filter

--- Exploration.R
+++ Exploration.R
@@ -1,4 +1,5 @@
 setwd("D:/Github/SwiftKey_Prediction")
+set.seed(42)
 
 require(tm)
 require(openNLP)
@@ -6,10 +7,28 @@ require(openNLP)
 data_blogs = readLines("Raw_Data/en_US/en_US.blogs.txt")
 data_twitter=readLines("Raw_Data/en_US/en_US.twitter.txt")
 data_news=readLines("Raw_Data/en_US/en_US.news.txt")
+
+#sample 1% of the data from each data set then combine together
+SampleData <- function(dataset, rate)
+{
+  return(dataset[as.logical(rbinom(length(dataset),1,rate))])
+}
+
+
+selectedData <- c(SampleData(data_blogs, 0.1),SampleData(data_twitter, 0.1),SampleData(data_news, 0.1))
+selectedData <- gsub("[^a-zA-Z ]", " ", selectedData)
+remove(data_blogs, data_news, data_twitter)
+
+#load profanity filter
 profanity <- read.csv("ProfanityWords.csv", header=FALSE)
 profanity <- profanity$V1
 
-zdata  <- Corpus(DirSource("D:/Github/SwiftKey_Prediction/Raw_Data/en_US/", encoding="UTF-8"), readerControl = list(language="en_US"))
+#remove all profanity words
+selectedData <- gsub(paste(profanity, collapse='|'), " ", selectedData)
+
 #zdata  <- Corpus(DirSource("D:/Github/SwiftKey_Prediction/Raw_Data/en_US/", encoding="UTF-8"), readerControl = list(language="en_US"))
+
+zdata <- Corpus(VectorSource(selectedData), readerControl = list(language="en_US"))
 zdata <- tm_map(zdata, stripWhitespace)
 zdata <- tm_map(zdata, content_transformer(tolower))
+
